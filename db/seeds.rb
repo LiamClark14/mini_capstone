180b9235ab7d20df1dd2ef6ef6d40e278e5953d1
LIAM CLARK <liamclark1717@gmail.com>
2020-03-03 18:41:58 -0600

    Added a few product models in the rails console

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,30 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+product_one = Product.new({
+  name: "Product 1",
+  price: 2.00,
+  image_url: "http://www.example.com/product_1",
+  description: "A beautiful product filled with value and history",
+})
+
+product_one.save
+
+product_two = Product.new({
+  name: "Product 2",
+  price: 1.00,
+  image_url: "http://www.example.com/product_2",
+  description: "A terrible product that we haven't managed to sell in 30 years",
+})
+
+product_two.save
+
+product_three = Product.new({
+  name: "Product 3",
+  price: 5.00,
+  image_url: "http://www.example.com/product_3",
+  description: "The best product we have ever sold, hands down",
+})
+
+product_three.save
